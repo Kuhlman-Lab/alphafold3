e72086446821a0dcc28511b37641d4be19ae0713
Nick Randolph (longleaf) <nzrandol@unc.edu>
2025-05-09 23:16:13 -0400

    Added examples 1A3B and 5DOB

---
+++ examples/example_5DOB/run_af3_mmseqs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH -J af3_mmseqs-5DOB
+#SBATCH -p a100-gpu
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem=16g
+#SBATCH -t 00-01:00:00
+#SBATCH --qos gpu_access
+#SBATCH --gres=gpu:1
+#SBATCH --output=slurm-%j-%x.out
+#SBATCH --error=slurm-%j-%x.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=user@email.com
+
+source ~/.bashrc
+module load gcc/12.2.0
+conda activate af3
+
+# This example makes a prediction for 5DOB with MMseqs MSA and templates.
+python /proj/kuhl_lab/alphafold3/run/run_af3.py \
+    --json_path ./alphafold_input.json \
+    --output_dir ./af3_preds_mmseqs \
+    --run_mmseqs
